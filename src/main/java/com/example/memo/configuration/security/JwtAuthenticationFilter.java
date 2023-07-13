93304d64cda0b83f76b9bfa21ede080d5c5a36eb
youngmin13 <wjsdudals56@naver.com>
2023-07-13 21:31:10 +0900

    1-2 시도

--- src/main/java/com/example/memo/configuration/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/memo/configuration/security/JwtAuthenticationFilter.java
@@ -1,17 +1,37 @@
 package com.example.memo.configuration.security;
 
+import com.example.memo.custom.CustomMemberInfo;
+import com.example.memo.domain.entity.Member;
 import com.example.memo.dto.LoginRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Set;
+
+import lombok.NoArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+	private JwtUtil jwtUtil;
+
+	public JwtAuthenticationFilter(JwtUtil jwtUtil) {
+		this.jwtUtil = jwtUtil;
+	}
+
+	public JwtAuthenticationFilter() {
+
+	}
 
 	@Override
 	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
@@ -33,6 +53,25 @@ class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 	@Override
 	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) {
 		// TODO : 올바른 인증 요청에 대한 결과로 jwt token 만들고, 검증한 후에 201 response로 해당 token 세팅하기
+		Member member = (Member)authResult.getPrincipal();
+		String username = member.getName();
+		String token = jwtUtil.createToken(username);
+
+		if (jwtUtil.validateToken(token)) {
+			try {
+				token = URLEncoder.encode(token, "utf-8").replaceAll("\\+", "%20");
+				try {
+					String authHeader = String.valueOf(jwtUtil.getClass().getField("AUTHORIZATION_HEADER"));
+					Cookie cookie = new Cookie(authHeader, token);
+					response.addCookie(cookie);
+					response.setStatus(201);
+				} catch (NoSuchFieldException e) {
+					throw new RuntimeException(e);
+				}
+			} catch (UnsupportedEncodingException e) {
+				throw new RuntimeException(e);
+			}
+		}
 	}
 
 	@Override
