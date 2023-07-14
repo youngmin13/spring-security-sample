2b065a8073237773773ed4f967f7e912e5c50827
youngmin13 <wjsdudals56@naver.com>
2023-07-14 09:49:57 +0900

    1-3 시도

--- src/main/java/com/example/memo/configuration/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/memo/configuration/security/JwtAuthenticationFilter.java
@@ -4,6 +4,8 @@ import com.example.memo.custom.CustomMemberInfo;
 import com.example.memo.domain.entity.Member;
 import com.example.memo.dto.LoginRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,9 +13,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Set;
 
 import lombok.NoArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -51,7 +56,7 @@ class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 	}
 
 	@Override
-	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) {
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException {
 		// TODO : 올바른 인증 요청에 대한 결과로 jwt token 만들고, 검증한 후에 201 response로 해당 token 세팅하기
 		Member member = (Member)authResult.getPrincipal();
 		String username = member.getName();
@@ -60,14 +65,10 @@ class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 		if (jwtUtil.validateToken(token)) {
 			try {
 				token = URLEncoder.encode(token, "utf-8").replaceAll("\\+", "%20");
-				try {
-					String authHeader = String.valueOf(jwtUtil.getClass().getField("AUTHORIZATION_HEADER"));
-					Cookie cookie = new Cookie(authHeader, token);
-					response.addCookie(cookie);
-					response.setStatus(201);
-				} catch (NoSuchFieldException e) {
-					throw new RuntimeException(e);
-				}
+				Map<String, Object> body = new LinkedHashMap<>();
+				body.put("token", token);
+				response.setStatus(201);
+				new ObjectMapper().writeValue(response.getOutputStream(), body);
 			} catch (UnsupportedEncodingException e) {
 				throw new RuntimeException(e);
 			}
--- src/main/java/com/example/memo/configuration/security/JwtAuthorizationFilter.java
+++ src/main/java/com/example/memo/configuration/security/JwtAuthorizationFilter.java
@@ -1,18 +1,42 @@
 package com.example.memo.configuration.security;
 
+import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+	private final String BEARER_PREFIX = "Bearer ";
+	private Authentication authenticationManager;
+	private JwtUtil jwtUtil;
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
 		FilterChain filterChain) throws ServletException, IOException {
 		// TODO : 요청에 들어온 JWT를 parsing해서 "ROLE_MEMBER" 권한이 있는지 확인하고, SecurityContextHolder에 context 설정하기
+		String token = resolveToken(request);
+		Claims info = jwtUtil.getUserInfoFromToken(token);
+		SecurityContextHolder.getContext().setAuthentication((Authentication) info);
 		filterChain.doFilter(request, response);
+	}
+
+	private String resolveToken(HttpServletRequest request) {
+		String bearerToken = request.getHeader(HttpHeaders.AUTHORIZATION);
+		if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
+			return bearerToken.substring(7);
+		}
+		return null;
 	}
 }
